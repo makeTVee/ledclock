a0c48d0fbf290e448d6902396b0f3967fb6ff459
Jason Coon <3598755+jasoncoon@users.noreply.github.com>
2023-01-06 08:31:37 -0600

    Made compatible with both PlatformIO and Arduino. Added github ci build actions. Reformatted code (pio vscode prettier).

---
+++ ci/build-arduino.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Enable the globstar shell option
+shopt -s globstar
+
+# Make sure we are inside the github workspace
+cd $GITHUB_WORKSPACE
+
+# Create directories
+mkdir $HOME/Arduino
+mkdir $HOME/Arduino/libraries
+
+# Install Arduino IDE
+export PATH=$PATH:$GITHUB_WORKSPACE/bin
+curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
+arduino-cli config init
+arduino-cli core update-index --additional-urls https://arduino.esp8266.com/stable/package_esp8266com_index.json
+
+# Install Arduino esp32 core
+arduino-cli core install esp32:esp32 --additional-urls https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
+
+# Install libraries
+arduino-cli lib install FastLED@3.5.0
+arduino-cli lib install AutoConnect@1.4.1
+arduino-cli lib install NTPClient@3.2.1
+
+arduino-cli compile --fqbn esp32:esp32:fm-devkit ./ledclock/ledclock.ino
