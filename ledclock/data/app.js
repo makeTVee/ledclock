1e4c233409f84ca9fa8a4082434f8cca368d1804
Jason Coon <3598755+jasoncoon@users.noreply.github.com>
2023-01-07 11:09:10 -0600

    Added color waves & palettes. Added more web UI controls: * Palette * Cycle Palette * Palette Duration * Pattern * Cycle Pattern * Pattern Duration * Speed Update web UI control visibility depending on current selections (mode & pattern).

--- ledclock/data/app.js
+++ ledclock/data/app.js
@@ -11,23 +11,78 @@ if (address.includes("localhost") || address.includes("127.0.0.1")) {
 document.addEventListener("DOMContentLoaded", onLoaded, false);
 
 let form;
-let modeInput;
+
+let modeSelect;
+
+let paletteSection;
+let paletteSelect;
+let cyclePaletteOnButton;
+let cyclePaletteOffButton;
+let paletteDurationInput;
+
+let patternSection;
+let patternSelect;
+let cyclePatternOnButton;
+let cyclePatternOffButton;
+let patternDurationInput;
+
+let speedSection;
+let speedInput;
+let speedInputRange;
+
+let clockSection;
 let timeOffsetInput;
+
 let statusDiv;
 let statusFooter;
 let statusSpinner;
 
 async function onLoaded() {
-  form = document.getElementById("form");
-  modeInput = document.getElementById("modeSelect");
   statusDiv = document.getElementById("statusDiv");
   statusFooter = document.getElementById("statusFooter");
   statusFooterSpinner = document.getElementById("statusFooterSpinner");
   statusSpinner = document.getElementById("statusSpinner");
-  timeOffsetInput = document.getElementById("timeOffsetInput");
+  form = document.getElementById("form");
 
-  modeInput.onchange = onModeChange;
+  modeSelect = document.getElementById("modeSelect");
+  modeSelect.onchange = onModeChange;
+
+  // pattern controls
+  patternSection = document.getElementById("patternSection");
+  patternSelect = document.getElementById("patternSelect");
+  patternSelect.onchange = onPatternChange;
+  cyclePatternOnButton = document.getElementById("cyclePatternOnButton");
+  cyclePatternOnButton.onclick = () => onCyclePatternChange(true);
+  cyclePatternOffButton = document.getElementById("cyclePatternOffButton");
+  cyclePatternOffButton.onclick = () => onCyclePatternChange(false);
+  patternDurationInput = document.getElementById("patternDurationInput");
+  patternDurationInput.onchange = onPatternDurationChange;
+
+  // speed controls
+  speedSection = document.getElementById("speedSection");
+  speedInput = document.getElementById("speedInput");
+  speedInput.onchange = onSpeedChange;
+  speedInputRange = document.getElementById("speedInputRange");
+  speedInputRange.onchange = onSpeedChange;
+
+  // palette controls
+  paletteSection = document.getElementById("paletteSection");
+  paletteSelect = document.getElementById("paletteSelect");
+  paletteSelect.onchange = onPaletteChange;
+  cyclePaletteOnButton = document.getElementById("cyclePaletteOnButton");
+  cyclePaletteOnButton.onclick = () => onCyclePaletteChange(true);
+  cyclePaletteOffButton = document.getElementById("cyclePaletteOffButton");
+  cyclePaletteOffButton.onclick = () => onCyclePaletteChange(false);
+  paletteDurationInput = document.getElementById("paletteDurationInput");
+  paletteDurationInput.onchange = onPaletteDurationChange;
+
+  // clock controls
+  clockSection = document.getElementById("clockSection");
+  timeOffsetInput = document.getElementById("timeOffsetInput");
   timeOffsetInput.onchange = onTimeOffsetChange;
+
+  loadPaletteOptions();
+  loadPatternOptions();
 
   const response = await fetch(`${urlBase}/data`);
   if (!response.ok) {
@@ -43,9 +98,37 @@ async function onLoaded() {
 
   console.log({ data });
 
-  const { mode, timeOffset } = data;
+  const {
+    mode,
 
-  modeInput.value = mode;
+    currentPaletteIndex,
+    cyclePalette,
+    paletteDuration,
+
+    currentPatternIndex,
+    cyclePattern,
+    patternDuration,
+
+    speed,
+
+    timeOffset,
+  } = data;
+
+  modeSelect.value = mode;
+  updateModeDisplay(mode);
+
+  patternDurationInput.value = patternDuration;
+  patternSelect.value = currentPatternIndex;
+  updateCyclePatternButtons(cyclePattern);
+  updatePatternDisplay(currentPatternIndex);
+
+  paletteDurationInput.value = paletteDuration;
+  paletteSelect.value = currentPaletteIndex;
+  updateCyclePaletteButtons(cyclePalette);
+
+  speedInput.value = speed;
+  speedInputRange.value = speed;
+
   timeOffsetInput.value = timeOffset / 60 / 60;
 
   form.style.display = "block";
@@ -58,7 +141,79 @@ async function onLoaded() {
 }
 
 async function onModeChange(ev) {
-  postValue("Mode", "mode", ev.target.selectedIndex);
+  const mode = ev.target.selectedIndex;
+  postValue("Mode", "mode", mode);
+  updateModeDisplay(mode);
+}
+
+function updateModeDisplay(mode) {
+  clockSection.style.display = mode === 1 ? "block" : "none";
+  patternSection.style.display = mode === 3 ? "block" : "none";
+}
+
+// pattern control event handlers
+function updateCyclePatternButtons(cyclePattern) {
+  cyclePatternOffButton.className = cyclePattern
+    ? "btn btn-outline-secondary"
+    : "btn btn-primary";
+  cyclePatternOnButton.className = cyclePattern
+    ? "btn btn-primary"
+    : "btn btn-outline-secondary";
+}
+
+async function onCyclePatternChange(cyclePattern) {
+  postValue("Cycle Pattern", "cyclePattern", cyclePattern ? 1 : 0);
+  updateCyclePatternButtons(cyclePattern);
+}
+
+async function onPatternDurationChange(ev) {
+  postValue("Pattern Duration", "patternDuration", ev.target.value);
+}
+
+async function onPatternChange(ev) {
+  const index = ev.target.selectedIndex;
+  postValue("Pattern", "currentPatternIndex", index);
+  updatePatternDisplay(index);
+}
+
+function updatePatternDisplay(index) {
+  const pattern = patterns[index];
+  paletteSection.style.display = pattern.palettes ? "block" : "none";
+  speedSection.style.display = pattern.speed ? "block" : "none";
+}
+
+// palette control event handlers
+function updateCyclePaletteButtons(cyclePalette) {
+  cyclePaletteOffButton.className = cyclePalette
+    ? "btn btn-outline-secondary"
+    : "btn btn-primary";
+  cyclePaletteOnButton.className = cyclePalette
+    ? "btn btn-primary"
+    : "btn btn-outline-secondary";
+}
+
+async function onCyclePaletteChange(cyclePalette) {
+  postValue("Cycle Palette", "cyclePalette", cyclePalette ? 1 : 0);
+  updateCyclePaletteButtons(cyclePalette);
+}
+
+async function onPaletteDurationChange(ev) {
+  postValue("Palette Duration", "paletteDuration", ev.target.value);
+}
+
+async function onPaletteChange(ev) {
+  postValue("Palette", "currentPaletteIndex", ev.target.selectedIndex);
+}
+
+async function onSpeedChange(ev) {
+  const speed = ev.target.value;
+  postValue("Speed", "speed", speed);
+  updateSpeedControls(speed);
+}
+
+function updateSpeedControls(speed) {
+  speedInput.value = speed;
+  speedInputRange.value = speed;
 }
 
 async function onTimeOffsetChange(ev) {
@@ -80,4 +235,88 @@ async function postValue(display, name, value) {
 
   statusFooterSpinner.style.display = "none";
   statusFooter.innerHTML = `Set ${display}: ${value}`;
+}
+
+function loadPaletteOptions() {
+  const paletteNames = [
+    "Rainbow",
+    "Rainbow Stripe",
+    "Cloud",
+    "Lava",
+    "Ocean",
+    "Forest",
+    "Party",
+    "Heat",
+    "Ice",
+    "Icy Blue",
+    "Snow",
+    "Red & White",
+    "Blue & White",
+    "Fairy",
+    "Retro C9",
+    "Red, Green & White",
+    "Holly",
+    "Sunset_Real",
+    "es_rivendell_15",
+    "es_ocean_breeze_036",
+    "rgi_15",
+    "retro2_16",
+    "Analogous_1",
+    "es_pinksplash_08",
+    "Coral_reef",
+    "es_ocean_breeze_068",
+    "es_pinksplash_07",
+    "es_vintage_01",
+    "departure",
+    "es_landscape_64",
+    "es_landscape_33",
+    "rainbowsherbet",
+    "gr65_hult",
+    "gr64_hult",
+    "GMT_drywet",
+    "ib_jul01",
+    "es_vintage_57",
+    "ib15",
+    "Fuschia_7",
+    "es_emerald_dragon_08",
+    "lava",
+    "fire",
+    "Colorfull",
+    "Magenta_Evening",
+    "Pink_Purple",
+    "es_autumn_19",
+    "BlacK_Blue_Magenta_White",
+    "BlacK_Magenta_Red",
+    "BlacK_Red_Magenta_Yellow",
+    "Blue_Cyan_Yellow",
+  ];
+
+  paletteNames.forEach((paletteName, index) => {
+    const option = document.createElement("option");
+    option.value = index;
+    option.text = paletteName;
+    paletteSelect.add(option);
+  });
+}
+
+const patterns = [
+  { name: "Spin", palettes: true },
+  { name: "Solid Palette", palettes: true },
+  { name: "Pride" },
+  { name: "Color Waves", palettes: true },
+  { name: "Rainbow" },
+  { name: "Rainbow With Glitter" },
+  { name: "Confetti" },
+  { name: "Sinelon", speed: true },
+  { name: "Juggle", speed: true },
+  { name: "BPM", speed: true },
+];
+
+function loadPatternOptions() {
+  patterns.forEach((pattern, index) => {
+    const option = document.createElement("option");
+    option.value = index;
+    option.text = pattern.name;
+    patternSelect.add(option);
+  });
 }
